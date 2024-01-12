e56471e8be8782c53059fb25c5363c31ec8f2e5b
Justin Davenport <justindavenport.space@gmail.com>
2024-01-11 23:05:09 -0500

    logic update

--- src/app/api/movieSearch/route.ts
+++ src/app/api/movieSearch/route.ts
@@ -3,13 +3,13 @@ export async function POST(request: Request) {
     const res = await request.json();
 
     const genre_key = await fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=4df29563fef1ac0f15e35abe376b0042&with_genres=${res.mainGenre},${res.subGenre1},${res.subGenre2}&vote_average.gte=6&page=${res.page}`
+      `https://api.themoviedb.org/3/discover/movie?api_key=4df29563fef1ac0f15e35abe376b0042&with_genres=${res.mainGenre},${res.subGenre1},${res.subGenre2}&vote_average.gte=6&vote_count.gte=1000&page=${res.page}`
     );
     const genre_data = await genre_key.json();
 
     let moviesArray = [];
 
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 20; i++) {
       const movie_key = await fetch(
         `https://www.omdbapi.com/?i=tt3896198&apikey=ed6233cd&t=${
           genre_data.results[
